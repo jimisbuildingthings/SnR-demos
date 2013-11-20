b54b2fbf9db54c11f5e4ca54e3f88c4c5873b6b8
Jim Czekalski <jim.czekalski@gmail.com>
2013-11-20 15:03:55 -0500

    Mortar recs demo
    Final version

--- mortar.py
+++ mortar.py
@@ -17,18 +17,35 @@ def index():
 	with open('initial_result.tsv', 'rb') as f:
     		mortarrecs = csv.reader(f, delimiter='\t', quotechar="|")
 		for row in mortarrecs:
-			rows.append(row)
-			playlistId = [row[1].replace('mgid:uma:videolist:mtv.com:','').split(',')]
-			recsId = [row[2].replace('mgid:uma:videolist:mtv.com:','').split(',')]
-			for l in playlistId:
+			newRow = []
+			userId = row[0]
+			content = []
+			recs = []
+			newRow.append(userId)
+			contentItems = [row[1].replace('mgid:uma:videolist:mtv.com:','').split(',')]
+			recsItems = [row[2].replace('mgid:uma:videolist:mtv.com:','').split(',')]
+			for l in contentItems:
 				for i in l:
 					url = 'http://api.mtv.com/api/c0K1gE5tQgoB/articles/' + str(i) + '/videos'
-					file = urllib2.urlopen(url)
-					data = file.read()
-					file.close()
+					f = urllib2.urlopen(url)
+					data = f.read()
+					f.close()
 					dom = parseString(data)
-					pTitle = dom.getElementsByTagName("headline")[0].toxml().replace('<headline>','').replace('</headline>','')
-		return render_template('index.html', rows=rows, pTitle=pTitle) 
+					cTitle = dom.getElementsByTagName("headline")[0].firstChild.nodeValue
+					content.append(cTitle)
+			newRow.append(content)
+			for l in recsItems:
+				for i in l:
+					url = 'http://api.mtv.com/api/c0K1gE5tQgoB/articles/' + str(i) + '/videos'
+					f = urllib2.urlopen(url)
+					data = f.read()
+					f.close()
+					dom = parseString(data)
+					rTitle = dom.getElementsByTagName("headline")[0].firstChild.nodeValue
+					recs.append(rTitle)
+			newRow.append(recs)
+			rows.append(newRow)
+		return render_template('index.html', rows=rows) 
 
 
 if __name__ == '__main__':
